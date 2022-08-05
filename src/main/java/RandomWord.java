3e34d87150d4f75799b9a205568b5c0f76330cc3
Nhan0504 <ttn0504ltt@gmail.com>
2022-08-05 14:23:59 +0700

    Use getResource method to open the filw of words

--- src/main/java/RandomWord.java
+++ src/main/java/RandomWord.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -10,8 +8,8 @@ public class RandomWord {
 
     public RandomWord (String fileName) {
         try {
-            File file = new File(fileName);
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    this.getClass().getClassLoader().getResourceAsStream(fileName)));
             while (reader.readLine() != null) {
                 wordList.add(reader.readLine());
             }
