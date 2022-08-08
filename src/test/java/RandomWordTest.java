ff61f16fe43792c5f65ef8e403b344b80232d1c5
Nhan0504 <ttn0504ltt@gmail.com>
2022-08-08 11:03:17 +0700

    Set the word to guess in each test

--- src/test/java/RandomWordTest.java
+++ src/test/java/RandomWordTest.java
@@ -7,27 +7,37 @@ import java.util.Arrays;
 public class RandomWordTest {
 
     RandomWord word = null;
+    UserGuess guess = null;
 
     @Before
     public void before() {
         word = new RandomWord("words.txt");
+        guess = new UserGuess();
+    }
+
+    @Test
+    public void testSetWord() {
         word.setWord("apple");
+        assertEquals("apple",word.getWord());
     }
 
     @Test
     public void testIsDone() {
+        word.setWord("apple");
         ArrayList<Character> curGuess = new ArrayList<>(Arrays.asList('a','p','p','l','e'));
         assertTrue(word.isDone(0, curGuess));
     }
 
     @Test
     public void testIsDoneWhenMissedMoreThanSix() {
+        word.setWord("apple");
         ArrayList<Character> curGuess = new ArrayList<>(Arrays.asList('a','p','p','l','e'));
         assertTrue(word.isDone(7, curGuess));
     }
 
     @Test
     public void testIsDoneWhenNotFinish() {
+        word.setWord("apple");
         ArrayList<Character> curGuess = new ArrayList<>(Arrays.asList('a','_','_','l','e'));
         assertFalse(word.isDone(0, curGuess));
     }
